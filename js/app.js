fb31b57721c313b0db71397707ba9c768f05a02c
nrh <nrh@wtnet.de>
2018-11-28 11:59:04 +0100

    Feat: Add card-uncover functionality

--- js/app.js
+++ js/app.js
@@ -12,19 +12,19 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +36,57 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+let deck = document.querySelector('.deck');
+//TESTEN
+//WORK IN PROGRESS
+
+//array to hold cards and counter variable
+let uncoveredCards = [];
+let numberOfCards = 0;
+
+//used for closing cards
+function closeCards(cards) {
+  for (let card of cards) {
+    card.classList.remove('open');
+    card.classList.remove('show');
+  }
+  cards.length = 0;
+  numberOfCards = 0;
+}
+//show Card functionality
+function showCard(target) {
+  event.target.classList.add('open'); //uncover Card
+  event.target.classList.add('show'); //by changing classList
+  uncoveredCards[numberOfCards] = event.target; //store card in array
+  numberOfCards++;
+}
+//main function for card-flip
+//as well as check for match
+function uncoverCard(event) {
+  if ((event.target.nodeName === 'LI')) {
+    if (uncoveredCards.length < 2) { //Only when an actual card is clicked
+      showCard(event.target);
+    }
+    //after 2 cards have been clicked:
+    if (uncoveredCards.length === 2) {
+      //check if the pictures on the cards match:
+      //AND if they are NOT the same card
+      if ((uncoveredCards[0].innerHTML == uncoveredCards[1].innerHTML) && (uncoveredCards[0] != uncoveredCards[1])) {
+        //if they match: make it show in the game by changing classes:
+        for (let card of uncoveredCards) {
+          card.classList.remove('open');
+          card.classList.remove('show');
+          card.classList.add('match');
+        }
+      }
+      //wait 1/2 second until closing cards - otherwise not visible
+      setTimeout(function() {
+        closeCards(uncoveredCards);
+      }, 500);
+    }
+  }
+}
+//adding an EventListener for whole card functionality
+deck.addEventListener('click', uncoverCard);
